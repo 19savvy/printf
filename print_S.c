1861d9ea051be21869f25cefa6d0f01a2137c767
Ken Nwogu <kenedynwogu027@gmail.com>
2023-03-28 00:44:16 -0700

    fix

--- print_S.c
+++ print_S.c
@@ -1,34 +1,40 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * print_S - A function that prints a string and nonprintable
- * character ascii values
- * @S: string to print
+ * print_S - A function that prints a string, with non-printable
+ * characters (0 < ASCII value < 32 or >= 127) represented in hexadecimal.
+ * @s: string to print
  * Return: number of printed characters
  */
-int print_S(va_list S)
+int print_S(va_list s)
 {
-	unsigned int i = 0;
-	int counter = 0;
-	char *str = va_arg(S, char *);
+    char *str, *hex;
+    int i, j, count = 0;
 
-	if (str == NULL)
-		str = "(null)";
-	for (; str[i]; i++)
-	{
-		if (str[i] < 32 || str[i] >= 127)
-		{
-			_putchar('\\');
-			_putchar('x');
-			counter += 2;
-			counter += print_x(S);
-		}
-		else
-		{
-			_putchar(str[i]);
-			counter++;
-		}
-	}
-	return (counter);
+    str = va_arg(s, char *);
+    if (str == NULL)
+        str = "(null)";
+
+    for (i = 0; str[i]; i++)
+    {
+        if (str[i] < 32 || str[i] >= 127)
+        {
+            printf("\\x");
+            hex = "0123456789ABCDEF";
+            j = str[i] / 16;
+            printf("%c", hex[j]);
+            j = str[i] % 16;
+            printf("%c", hex[j]);
+            count += 4;
+        }
+        else
+        {
+            putchar(str[i]);
+            count++;
+        }
+    }
+    return (count);
 }
+
